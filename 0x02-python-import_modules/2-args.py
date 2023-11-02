12f2ced5e07cdbdcb907a057117b8cb1f19ee341
Nardos <contactnardos@gmail.com>
2023-11-02 10:31:19 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
 argvec = sys.argv
 arglen = len(sys.argv)- 1
 if arglen == 0:
@@ -12,4 +11,4 @@ else:
     else:
         print("{} arguments:".format(arglen))
     for i in range(arglen):
-            print("{}: {}".format(i + 1 , argvec[i + 1]))
+        print("{}: {}".format(i + 1 , argvec[i + 1]))
