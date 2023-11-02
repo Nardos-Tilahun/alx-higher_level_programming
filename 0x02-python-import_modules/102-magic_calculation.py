e75b729071350eeadd5e9253f972b40c4f1b4436
Nardos <contactnardos@gmail.com>
2023-11-02 13:53:28 +0000

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-def magic_calcualation(a, b)
-{
-    import magic_calculation_102 from add, sub as add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
-    return 0
-}
+import magic_calculation_102 from add, sub as add, sub
+if a < b:
+    c = add(a, b)
+    for i in range(4, 6):
+        c = add(c, i)
+    return c
+else:
+    return sub(a, b)
+return 0
