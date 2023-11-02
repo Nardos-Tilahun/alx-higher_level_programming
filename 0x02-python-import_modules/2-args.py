6b681c96a28f7bef7170b885ea6c4e4d653ef1e1
Nardos <contactnardos@gmail.com>
2023-11-02 10:22:01 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+argvec = sys.argv
+arglen = len(sys.argv)
+if arglen == 1:
+    print("{} arguments.".format(arglen - 1))
+else:
+    if arglen == 2:
+        print("{} argument:".format(arglen - 1))
+    else:
+        print("{} arguments:".format(arglen - 1))
+    for i in range(len(argvec)):
+        if i > 0:
+            print("{}: {}".format(i , argvec[i]))
