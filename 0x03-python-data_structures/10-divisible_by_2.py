5ea9f8ea61b71c0ebc67ed57f5288288a7dd3a08
Nardos <contactnardos@gmail.com>
2023-11-04 08:22:29 +0000

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
+    newli = []
+    for i in my_list:
+        if i % 2 == 0:
+            newli.append(True)
+        else:
+            newli.append(False)
+    return newli              
