8ac0328974c3ec47b1efdd578b72a3d12eaac4ce
Nardos <contactnardos@gmail.com>
2023-11-04 10:35:14 +0000

    12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-if a is not None and b is not None:
-    a, b = int(b), int(a)
-    print("a={:d} - b={:d}".format(a, b))
+a, b = int(b), int(a) if a is not None and b is not None else (a, b)
+print("a={:d} - b={:d}".format(a, b))
