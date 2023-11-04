c398a145479115dae3e724a1f473398963aeaed4
Nardos <contactnardos@gmail.com>
2023-11-04 05:05:45 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if my_string[i] == 'c':
-            my_string[i] = 'C'
-    return my_string
+    my_copy = my_string.copy()
+    for i in range(len(my_copy)):
+        if my_copy[i] == 'c':
+            my_copy[i] = 'C'
+    return my_copy
