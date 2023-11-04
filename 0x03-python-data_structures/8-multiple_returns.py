f767659eaa20ffb6a4b5256cb777c58960b3e7e3
Nardos <contactnardos@gmail.com>
2023-11-04 07:33:19 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is not None:
+    if sentence is not {}:
         return len(sentence), sentence[0]
+    else:
+        return 0, None
