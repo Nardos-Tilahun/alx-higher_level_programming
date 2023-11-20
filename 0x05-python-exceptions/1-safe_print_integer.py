5ead9eacb144d0838b38fd122dc66c3ffe126998
Nardos <contactnardos@gmail.com>
2023-11-20 08:01:43 +0000

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        a = int(value)
+    except ValueError:
+        return (False)
+    else:
+        print("{:d}".format(value))
+        return (True)
