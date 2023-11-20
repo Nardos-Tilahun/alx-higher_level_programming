32e8667e7333eee651ee7a832e21ab88a4c8ccf4
Nardos <contactnardos@gmail.com>
2023-11-20 10:05:38 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         a = int(value)
-    except ValueError:
-        return (False)
-    else:
         print("{:d}".format(value))
         return (True)
+    except ValueError:
+        return (False)
