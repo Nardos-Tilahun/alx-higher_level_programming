8c0125654b2c170987f750021ab65fe8b3a2df74
Nardos <contactnardos@gmail.com>
2023-11-21 16:24:59 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is a module to write a class Square"""
+
+
+class Square:
+    """ This is a class that define square with size of private"""
+    def __init__(self, size):
+        """ this initialize the class Square with the size"""
+        self.__size = size
