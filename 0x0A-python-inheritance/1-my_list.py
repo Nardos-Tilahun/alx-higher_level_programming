13ce8142e97ffc06e07d873de010339d7208b4a5
Nardos <contactnardos@gmail.com>
2023-12-05 02:32:37 +0000

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' this is  1-my_list
+'''
+
+
+class MyList(list):
+    '''  MyList class to print the sorted list
+    '''
+
+    def print_sorted(self):
+        '''
+        prints the sorted list
+        '''
+        print(sorted(self))
