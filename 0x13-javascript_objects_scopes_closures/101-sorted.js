7aa9bc8934fb874ac738b12564cf31e2f046155e
Nardos <contactnardos@gmail.com>
2024-02-24 09:31:03 +0000

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
-// function that returns the number of occurrences in a list
+const { dict } = require('./101-data');
 
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
-});
-console.log(mappedList);
+// Function to invert the dictionary
+function invertDictionary (dict) {
+  const invertedDict = {};
+  for (const userId in dict) {
+    const occurrences = dict[userId];
+    if (!invertedDict[occurrences]) {
+      invertedDict[occurrences] = [];
+    }
+    invertedDict[occurrences].push(userId);
+  }
+  return invertedDict;
+}
+
+// Compute the inverted dictionary
+const invertedDict = invertDictionary(dict);
+
+// Print the inverted dictionary
+console.log(invertedDict);
