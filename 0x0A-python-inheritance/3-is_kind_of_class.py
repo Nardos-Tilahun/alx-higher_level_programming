6386e01ec78cb70abf81f8c3d7027ff4981a08d5
Nardos <contactnardos@gmail.com>
2024-02-24 09:47:29 +0000

    gitignore

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,7 @@
 def is_kind_of_class(obj, a_class):
     '''function: is_kind_of_class
     obj: an object
-    a_class: a class
+    a_class: a class 
     Returns: Bool
     '''
     return isinstance(obj, a_class)
