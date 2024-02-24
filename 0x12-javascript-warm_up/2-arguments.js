f4eb12378aee1a9b5daea972b44518be54eb39a9
Nardos <contactnardos@gmail.com>
2024-02-24 07:20:37 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const argv = process.argv;
 if (argv.slice(2).length === 0) {
   console.log('No argument');
+} else if (argv.slice(2).length === 1) {
+  console.log('Argument found');
 } else {
-  console.log('Argument Found');
+  console.log('Arguments found');
 }
