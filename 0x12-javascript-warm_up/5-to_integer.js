631dcb2aea5dd7d1bac32054812420848a5b070f
Nardos <contactnardos@gmail.com>
2024-02-24 07:42:50 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const firstArg = process.argv[2];
 
 // Check if the argument can be converted to an integer using isNaN
 if (!isNaN(Number(firstArg))) {
-  console.log(`My number: ${parseInt (Number(firstArg))}`);
+  console.log(`My number: ${parseInt(Number(firstArg))}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
