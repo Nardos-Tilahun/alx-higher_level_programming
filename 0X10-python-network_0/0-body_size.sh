e9907421ac6986f83712cc1e4118dc04b3f537e6
Nardos T <contactnardos@gmail.com>
2024-07-18 13:13:58 +0300

    nothing

--- 0X10-python-network_0/0-body_size.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script takes a URL, sends a request, and displays the size of the response body in bytes
-
-size=$(curl -s "$1" | wc -c)
-echo "$size"
-
--- 0X10-python-network_0/README.md
+++
@@ -1,1 +0,0 @@
-This is a pythton Network 0 class
