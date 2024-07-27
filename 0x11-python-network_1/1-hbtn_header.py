47d43320b44d7ff478d636b565b6cdf4af54216a
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 19:44:12 +0300

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,5 @@ from urllib.request import Request, urlopen
 if __name__ == "__main__":
     url = argv[1]
     req = Request(url)
-    
     with urlopen(req) as response:
         print(dict(response.headers).get("X-Request-Id"))
