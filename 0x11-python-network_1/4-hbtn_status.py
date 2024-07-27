9a5b75d93a5cb35b1aca1f9676cd4eb9d6af8341
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 20:17:42 +0300

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-"""
+"""Write a Python script that fetches
+https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    req = request.get("https://alx-intranet.hbtn.io/status")
+    print("Body Response:")
+    print("\t- type: {}"format(type(req.text)))
+    print("\t- content: {}"format(req.text))
