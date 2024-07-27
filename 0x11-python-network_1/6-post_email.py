f57d50d842937e8f06196672b92d02b9766c290e
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 20:39:10 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,10 +7,9 @@ from sys import argv
 import requests
 
 
-if __name___ == "__main__":
+if __name__ == "__main__":
     url = argv[1]
     value = {"email": argv[2]}
     req = requests.post(url, data=value)
 
     print(req.text)
-
