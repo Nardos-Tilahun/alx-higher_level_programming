c697bc7300f20ee14d8b97a13883394a100ad154
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 19:57:03 +0300

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter"""
+""" script that sends a POST request to the passed URL with the email as a parameter"""
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 from sys import argv
