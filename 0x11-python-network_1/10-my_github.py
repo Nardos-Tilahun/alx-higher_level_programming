7beb49ec16c9f281d3bf586974a6dba3979e3037
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 21:16:42 +0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
     auth = HTTPBasicAuth(argv[1], argv[2])
     req = requests.get("https://api.github.com/user", auth=auth)
 
-    print(req.json().get("id"))
-    
+    print(req.json().get("id")) 
