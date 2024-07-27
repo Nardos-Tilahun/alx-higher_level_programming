d79331b36a7548f24f607ebeb642d99c1ff2bce9
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 21:05:24 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,8 +8,8 @@ import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(argv) ==1 else argv[1]
-    req = requests.post("http://0.0.0.0:5000/search_user", {"q":letter})
+    letter = "" if len(argv) == 1 else argv[1]
+    req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
 
     try:
         response = req.json()
