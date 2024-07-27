7653757efc65715effe63d0b38326299c26f512d
Nardos Dubale <nardos@Nardos-Kali.nardosdubale>
2024-07-27 19:54:32 +0300

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", replace))
